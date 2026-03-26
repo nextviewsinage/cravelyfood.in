8015c6ed3bac3e0d07adc2ccbfb354715ea3528f
Trakky Dev <dev@trakky.in>
2026-03-27 00:32:46 +0530

    fix: disable browsable API

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -114,6 +114,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 # ── JWT ───────────────────────────────────────────────
