dfdd53904053dd07ec516d162638f7b875f9c241
Trakky Dev <dev@trakky.in>
2026-03-27 00:04:01 +0530

    fix: add Vercel URLs to CORS

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -1,3 +1,14 @@
 from django.urls import path
+from django.http import HttpResponse
 
-urlpatterns = []
+def index(request):
+    return HttpResponse(
+        "<h2>🍕 FoodDelivery API is running!</h2>"
+        "<p>Frontend: <a href='http://localhost:3000'>http://localhost:3000</a></p>"
+        "<p>API: <a href='/api/foods/'>/api/foods/</a></p>"
+        "<p>Admin: <a href='/admin/'>/admin/</a></p>"
+    )
+
+urlpatterns = [
+    path('', index),
+]
